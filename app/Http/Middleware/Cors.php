3f5b6302a90aac05a1c0c25dc22da213207809e5
elias182 <122615280+elias182@users.noreply.github.com>
2024-06-16 22:13:02 +0200

    Update Cors.php

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -9,7 +9,7 @@ class Cors
     public function handle($request, Closure $next)
     {
         return $next($request)
-            ->header('Access-Control-Allow-Origin', 'http://localhost:3000')
+            ->header('Access-Control-Allow-Origin', 'https://front-us-production.up.railway.app')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
             ->header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Authorization');
     }
